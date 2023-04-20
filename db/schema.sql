6ff21baf864e04e9cdc20e612d7fa52944e7cc20
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-04-20 13:15:29 -0700

    added inquirer prompts and functions

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,27 @@
-
+-- create emoployee database to use for tables --
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
 
 
 -- create department table --
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  dept_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (deptment_id)
+  id INT NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 -- create role table --
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
-    -- do  i need references on foreign key --
+   
 );
+
 
 -- create employee table --
 CREATE TABLE employee (
--- server.js
+++ server.js
@@ -11,13 +11,15 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 //require console.table
-const cTable = require('console.table');
+//const cTable = require('console.table');
 
 //require mysql12
 const mysql = require('mysql2');
 
+
+
 //port
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 //call express function varibale
 const app = express();
@@ -26,19 +28,288 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+
 //connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
         // MySQL username,
-        user: process.env.USER,
+        user: 'root',
         // MySQL password
         password: process.env.PASSWORD,
         // company database
-        database: 'employees_db',
+        database: 'employee_db',
         // port
-        port: 3001,
+        port: 3306,
     },
     console.log(`Connected to the company database.`)
 );
-        
+
+
+//start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
+
+//inquirer prompt
+const question = inquirer.prompt
+    ({
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'View all employees',
+            'View all employees by department',
+            'View all employees by manager',
+            'Add employee',
+            'Remove employee',
+            'Update employee role',
+            'Update employee manager',
+            'View all roles',
+            'Add role',
+            'Remove role',
+            'View all departments',
+            'Add department',
+            'Remove department',
+            'View the total utilized budget of a department',
+            'Exit'
+        ]
+    })
+    .then((answer) => {
+        switch (answer.action) {
+            case 'View all employees':
+                viewEmployees();
+                break;
+
+            case 'View all employees by department':
+                viewEmployeesByDepartment();
+                break;
+
+            case 'View all employees by manager':
+                viewEmployeesByManager();
+                break;
+
+            case 'Add employee':
+                addEmployee();
+                break;
+
+            case 'Remove employee':
+                removeEmployee();
+                break;
+
+            case 'Update employee role':
+                updateEmployeeRole();
+                break;
+
+            case 'Update employee manager':
+                updateEmployeeManager();
+                break;
+
+            case 'View all roles':
+                viewRoles();
+                break;
+
+            case 'Add role':
+                addRole();
+                break;
+
+            case 'Remove role':
+                removeRole();
+                break;
+
+            case 'View all departments':
+                viewDepartments();
+                break;
+
+            case 'Add department':
+                addDepartment();
+                break;
+
+            case 'Remove department':
+                removeDepartment();
+                break;
+
+            case 'View the total utilized budget of a department':
+                viewBudget();
+                break;
+
+            case 'Exit':
+                exit();
+                break;
+        }
+    }
+    );
+
+//view all employees
+function viewEmployees() {
+    db.query(`SELECT * FROM employee`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//view all employees by department
+function viewEmployeesByDepartment() {
+    db.query(`SELECT * FROM department`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//view all employees by manager
+function viewEmployeesByManager() {
+    db.query(`SELECT * FROM employee`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//add employee
+function addEmployee() {
+    db.query(`SELECT * FROM employee`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//remove employee
+function removeEmployee() {
+    db.query(`SELECT * FROM employee`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//update employee role
+function updateEmployeeRole() {
+    db.query(`SELECT * FROM employee`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//update employee manager
+function updateEmployeeManager() {
+    db.query(`SELECT * FROM employee`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//view all roles
+function viewRoles() {
+    db.query(`SELECT * FROM role`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+
+//add role
+function addRole() {
+    db.query(`SELECT * FROM role`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//remove role
+function removeRole() {
+    db.query(`SELECT * FROM role`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//view all departments
+function viewDepartments() {
+    db.query(`SELECT * FROM department`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//add department
+function addDepartment() {
+    db.query(`SELECT * FROM department`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//remove department
+function removeDepartment() {
+    db.query(`SELECT * FROM department`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//view budget
+function viewBudget() {
+    db.query(`SELECT * FROM department`, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        start();
+    });
+}
+
+//exit
+function exit() {
+    process.exit();
+}
+
+//start
+function start() {
+    question;
+}
+
+
+
+
